59cc5e239a329375db0e6a220fa71420271c5bf0
Gaberdugo <gaberdugov@unal.edu.co>
2024-11-25 02:49:41 -0500

    Arreglando Requirements 8

--- Backend/core/settings.py
+++ Backend/core/settings.py
@@ -57,6 +57,7 @@ CKEDITOR_CONFIGS = {
         'autoParagraph': False
     }
 }
+
 CKEDITOR_UPLOAD_PATH = "/media/"
 
 MIDDLEWARE = [
@@ -142,15 +143,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
-
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'build/static')
+    os.path.join(BASE_DIR, 'static'),  # Asegúrate de que la carpeta "static" exista
 ]
+
+# Archivos de medios (para archivos subidos)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
