29ef5e38472542033e27a5fc2f57770af3e03098
Gaberdugo <gaberdugov@unal.edu.co>
2024-11-26 08:03:45 -0500

    Actualización

--- Backend/core/settings.py
+++ Backend/core/settings.py
@@ -99,12 +99,17 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+}"""
+
+DATABASES = {
+    'default': env.db("DATABASE_URL")
 }
+DATABASES["default"]["ATOMIC_REQUEST"] = True
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
