bd240caace1f0e0631671db5a3d169ae783b6cfb
Gustavo <gaberdugov@unal.edu.co>
2024-12-01 22:31:42 -0500

    Para Render 2

--- Backend/core/settings.py
+++ Backend/core/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS_DEV')
+ALLOWED_HOSTS = ['*']
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
   
