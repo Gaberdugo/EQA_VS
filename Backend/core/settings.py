509a22a2f64ba9a7e0a00669a1ebdd3e1616eb2f
Gustavo <gaberdugov@unal.edu.co>
2024-12-02 16:48:30 -0500

    Correcciones db 2

--- Backend/core/settings.py
+++ Backend/core/settings.py
@@ -125,6 +125,10 @@ DATABASES = {
     }
 }
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://eqa-vs.azurewebsites.net',
+]
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
