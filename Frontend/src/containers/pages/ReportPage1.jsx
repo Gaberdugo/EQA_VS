c120de3dbe730d0fcf2d303f2fbab994a0829f45
Gustavo <gaberdugov@unal.edu.co>
2025-04-07 19:30:15 -0500

    Actualización Reportes React 3

--- Frontend/src/containers/pages/ReportPage1.jsx
+++ Frontend/src/containers/pages/ReportPage1.jsx
@@ -75,7 +75,7 @@ function ReportPage() {
     return (
         <Layout4>
             <div style={styles.container}>
-                <h1 style={styles.title}>Generador de Reportes Generales</h1>
+                <h1 style={styles.title}>Generador de Reportes por Institución</h1>
 
                 {/* Selector de proyectos */}
                 <div style={styles.selectContainer}>
