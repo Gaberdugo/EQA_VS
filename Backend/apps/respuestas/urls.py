51dceec173f4d9195cd6f031c34f8c83247d62d9
Gustavo <gaberdugov@unal.edu.co>
2025-04-14 18:41:32 -0500

    Actualización Reportes Main 14

--- Backend/apps/respuestas/urls.py
+++ Backend/apps/respuestas/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from .views import EncuestaView, EncuestaExportView, CuadernilloMateAPIView, EncuestaAleatoriaView, PreguntaView, CuadernillosNombresAPIView, EncuestaDeleteView, EncuestasResponsableView
-from .views import ObtenerEncuestaAPIView, ModificarEncuestaAPIView, ObtenerInstitucionesAPIView,ObtenerMunicipiosAPIView
+from .views import ObtenerEncuestaAPIView, ModificarEncuestaAPIView, ObtenerInstitucionesAPIView,ObtenerMunicipiosAPIView,GenerarReporte1APIIew
 
 urlpatterns = [
     path('respuesta/', EncuestaView.as_view(), name='repuesta_create'),  # Endpoint para crear la encuesta
     path('respuesta/exportar/', EncuestaExportView.as_view(), name='encuestas_export'),  # Endpoint para exportar encuestas a Excel
     path('instituciones/', ObtenerInstitucionesAPIView.as_view(), name='obtener_instituciones'),
     path('municipios/', ObtenerMunicipiosAPIView.as_view(), name='obtener_municipios'),
+    path('pdf/', GenerarReporte1APIIew.as_view(), name='obtener_pdf'),
     path('cuadernillos/', CuadernilloMateAPIView.as_view(), name='cuadernillo_list'), 
     path('respuesta/aleatoria/', EncuestaAleatoriaView.as_view(), name='encuesta-aleatoria'),
     path("preguntas/", PreguntaView.as_view(), name="preguntas"),
