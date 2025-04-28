94b9749b19d8dcb5cc23cb513c1dc6fc623ff49d
Gustavo <gaberdugov@unal.edu.co>
2025-04-28 17:29:05 -0500

    Actualización Reportes Main 74

--- Backend/apps/respuestas/views.py
+++ Backend/apps/respuestas/views.py
@@ -619,10 +619,11 @@ class GenerarReporte1APIIew(APIView):
             descripcion_texto = '<b>2. Ficha técnica: número de estudiantes matriculados y evaluados</b>'
 
             elements.append(Paragraph(descripcion_texto, descripcion_izq_style))
-
-
-
+            
+            
             elements.append(PageBreak())
+
+            """
 
             #----------------------------------------------------------------------------------------------------------------------------
 
@@ -630,7 +631,7 @@ class GenerarReporte1APIIew(APIView):
 
             elements.append(Paragraph(descripcion_texto, descripcion_izq_style))
 
-            contenido = """
+            contenido = ""
             <b><font color='#1B8830'>¿Qué se evalúa:</font></b><br/><br/>
             Las pruebas de Lenguaje evalúan las habilidades de los estudiantes de tercero y quinto grados para interpretar y comprender diversos tipos y formatos de textos orientados a diferentes propósitos.<br/><br/>
             Los tipos de textos evaluados son los siguientes: narrativos, descriptivos, dialogales, explicativos y argumentativos.<br/><br/>
@@ -640,7 +641,7 @@ class GenerarReporte1APIIew(APIView):
             • <b>Inferencial:</b> implica reconocer el significado implícito de los contenidos en un texto.<br/>
             • <b>Crítica:</b> implica evaluar los contenidos y las formas de los textos, así como hacer una valoración de argumentos.<br/><br/>
             Los puntajes en esta prueba se presentan en una escala de 0 a 20 puntos. 
-            """
+            ""
 
             contenido_parrafo = Paragraph(contenido, recuadro_style)
             recuadro_tabla = Table([[contenido_parrafo]], colWidths=[460])
@@ -926,7 +927,7 @@ class GenerarReporte1APIIew(APIView):
 
             elements.append(Paragraph(descripcion_texto, descripcion_izq_style))
 
-            contenido = """
+            contenido = ""
             <b><font color='#1B8830'>¿Qué se evalúa:</font></b><br/><br/>
             Las pruebas de Matemáticas evalúan las habilidades de los estudiantes de tercero y quinto grados para plantear y resolver 
             diferentes tipos de problemas matemáticos teniendo en cuenta las siguientes competencias y componentes, establecidos en los 
@@ -939,7 +940,7 @@ class GenerarReporte1APIIew(APIView):
             • Númerico - variacional<br/>
             • Espacial - métrico<br/>
             • Aleatorio<br/><br/>
-            """
+            ""
 
             contenido_parrafo = Paragraph(contenido, recuadro_style)
             recuadro_tabla = Table([[contenido_parrafo]], colWidths=[460])
@@ -1218,7 +1219,7 @@ class GenerarReporte1APIIew(APIView):
 
             elements.append(Spacer(1, 12))
             elements.append(tabla_niveles)
-            
+            """
             #-----------------------------------------------------------------------------------------------------------------------
 
             # Crear documento base
