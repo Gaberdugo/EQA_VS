ca41ff2f1e782126a60f1a1073125fdac62ad63d
Gustavo <gaberdugov@unal.edu.co>
2025-05-19 11:31:31 -0500

    Lista de usuarios main 3

--- Backend/apps/proyecto/serializers.py
+++ Backend/apps/proyecto/serializers.py
@@ -14,7 +14,7 @@ class DepartamentoSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Departamento
-        fields = ['id','nombre']
+        fields = ['id','nombre', 'ciudades']
 
 class ProyectoSerializer(serializers.ModelSerializer):
     ciudades = CiudadSerializer(many=True, read_only=True)
