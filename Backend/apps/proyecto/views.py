6960e69ea1bac4d5f0f656dde72e419972040f48
Gustavo <gaberdugov@unal.edu.co>
2025-05-19 12:27:32 -0500

    Lista de usuarios main 5

--- Backend/apps/proyecto/views.py
+++ Backend/apps/proyecto/views.py
@@ -3,20 +3,24 @@ from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
 from .models import Ciudad, Departamento, Proyecto
 from .serializers import CiudadSerializer, DepartamentoSerializer, ProyectoSerializer
 
 # Vista para el modelo Departamento
 class DepartamentoViewSet(viewsets.ModelViewSet):
+    permission_classes = [AllowAny]
     queryset = Departamento.objects.all()
     serializer_class = DepartamentoSerializer
 
 # Vista para el modelo Ciudad
 class CiudadViewSet(viewsets.ModelViewSet):
+    permission_classes = [AllowAny]
     queryset = Ciudad.objects.all()
     serializer_class = CiudadSerializer
 
 class ProyectoViewSet(APIView):
+    permission_classes = [AllowAny]
     def get(self, request):
         # Obtenemos todos los proyectos
         proyectos = Proyecto.objects.all()
