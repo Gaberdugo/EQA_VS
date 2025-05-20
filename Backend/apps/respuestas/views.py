e74aa3049aeb8a172c38dea42f902c7bd69c4e31
Gustavo <gaberdugov@unal.edu.co>
2025-05-19 21:47:24 -0500

    Cambios Reportes por revisión 11

--- Backend/apps/respuestas/views.py
+++ Backend/apps/respuestas/views.py
@@ -1043,8 +1043,8 @@ class GenerarReporte1APIIew(APIView):
 
             # Crear gráfico
             plt.figure(figsize=(6, 5))
-            bars1 = plt.bar([i - bar_width/2 for i in x], t, width=bar_width*2, label='Institución', color='#1B8830')
-            bars2 = plt.bar([i + bar_width/2 for i in x], c, width=bar_width*2, label='Ciudad', color='#A4D7B2')
+            bars1 = plt.bar([i - bar_width/2 for i in x], t, width=bar_width, label='Institución', color='#1B8830')
+            bars2 = plt.bar([i + bar_width/2 for i in x], c, width=bar_width, label='Ciudad', color='#A4D7B2')
 
             # Agregar etiquetas encima de las barras
             for i, bar in enumerate(bars1):
@@ -1058,7 +1058,7 @@ class GenerarReporte1APIIew(APIView):
             # Ajustes del gráfico
             plt.xticks(x, niveles)
             plt.title('')
-            plt.legend()
+            plt.legend(fontsize=14)
             plt.tight_layout()
 
             # QUITAR eje Y
