9d6cb5391f186bc0e3ccd046e55c052811960c77
Gustavo <gaberdugov@unal.edu.co>
2025-05-20 22:53:31 -0500

    Main cambios 14

--- Backend/core/wsgi.py
+++ Backend/core/wsgi.py
@@ -3,6 +3,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 setting_module = 'core.settings'
 
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', setting_module)
 
 application = get_wsgi_application()
