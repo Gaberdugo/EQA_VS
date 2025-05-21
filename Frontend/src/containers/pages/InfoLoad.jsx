cc33264dca617cd0d803263d1edab34d7bfbb1e3
Gustavo <gaberdugov@unal.edu.co>
2025-05-21 09:15:14 -0500

    React cambios 5

--- Frontend/src/containers/pages/InfoLoad.jsx
+++ Frontend/src/containers/pages/InfoLoad.jsx
@@ -158,10 +158,8 @@ function InfoLoad() {
       ...respuestasObj,
     };
 
-    console.log(proyectoData.nombre_institucion);
-
     try {
-      await axios.post(`${process.env.REACT_APP_API_URL}/info/proyectos/`, proyectoData);
+      await axios.post(`${process.env.REACT_APP_API_URL}/res/respuesta/`, proyectoData);
       setMensajeExito('La información fue cargada correctamente.');
       setTimeout(() => {
         setFormData(prev => ({
